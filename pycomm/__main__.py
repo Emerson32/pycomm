f4850dca44496d63d3515df319e2c193adcb2139
Emerson <Houndlover32@gmail.com>
2019-07-25 11:01:19 -0400

    Added screen clearing functionality for entry point

--- pycomm/__main__.py
+++ pycomm/__main__.py
@@ -2,26 +2,22 @@
 # __main__.py - Entry point for client application
 
 import click
-import sys
 
 from pycomm.commands.chat import chat
 from pycomm.commands.create_service import create_service
-from pycomm.commands.transfer import transfer
 from pycomm.commands.reverse_shell import shell
+from pycomm.commands.transfer import transfer
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.version_option(version='1.0.0', prog_name='pycomm')
-@click.group(invoke_without_command=True,
-             context_settings=CONTEXT_SETTINGS)
+@click.group(context_settings=CONTEXT_SETTINGS)
 def main():
-    if len(sys.argv) == 1:
-        raise click.UsageError('Missing option or subcommand')
+    click.clear()
 
 
 main.add_command(chat)
 main.add_command(create_service)
 main.add_command(shell)
 main.add_command(transfer)
-
